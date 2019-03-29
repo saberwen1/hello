ff5b4f948d5fd9fa13ed25bf3796a7d0ed130a00
唐文文 <18365284289@163.com>
2019-03-29 14:34:57 +0800

    auto publish

--- src/main/java/com/fate/hello/controller/HelloController.java
+++ src/main/java/com/fate/hello/controller/HelloController.java
@@ -3,6 +3,9 @@ package com.fate.hello.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @Author: fate
  * @Date: 2019/3/28 16:27
@@ -14,5 +17,12 @@ public class HelloController {
     @RequestMapping("/")
     public String hello() {
         return "hello world";
+    }
+
+    @RequestMapping("/rest")
+    public List rest() {
+        String[] strings = {"tom","cost","3","days","learning"};
+        List<String> list = Arrays.asList(strings);
+        return list;
     }
 }
