bff7b084665680c39816dc5f4ae7177cb5cbb76f
唐文文 <18365284289@163.com>
2019-03-29 12:11:48 +0800

    reset

--- src/main/java/com/fate/hello/controller/HelloController.java
+++ src/main/java/com/fate/hello/controller/HelloController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping
+    @RequestMapping("/")
     public String hello() {
         return "hello world";
     }
